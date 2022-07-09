214a9c8775783a846fe893bd408142b834efab90
Liwis779 <95344837+Liwis779@users.noreply.github.com>
2022-07-09 22:43:06 +0200

    gestion de la gare
    Première version du système de gestion de la gare

---
+++ gare.lua
@@ -0,0 +1,101 @@
+local component = require("component")
+local j 		= require("json")
+local event 	= require("event")
+local io 		= require("io")
+
+local r = component.redstone
+local m = component.modem
+
+local direction  = 0
+local action 	 = 0
+local gare 		 = 0
+local table 	 = 0
+local aiguillage = 0
+
+function getFile(name)
+	temp = 0
+	file = io.open(name,"r")
+	temp = file:read()
+	file:close()
+	temp = j.decode(temp)
+	return temp
+end
+
+function checkQuai()
+	for n = 1,gare["nbQuai"] do
+		if r.getBundledInput(5,gare["quai"][n]["detecR"]) == 0 then 
+			gare["quai"][n]["full"] = false
+		else
+			gare["quai"][n]["full"] = true
+		end
+	end
+end
+
+function giveQuai()
+	found = false
+	for n=1,gare["nbQuai"] do
+		if gare["quai"][n]["full"] == false then
+			found = true
+			num = n
+			break
+		end
+	end
+	if found == true then
+		return num
+	else
+		return "no station free"
+	end
+end
+
+function rooting(voie, quai)
+	for n = 1, aiguillage["nbAig"] do
+		r.setBundledOutput(5,aiguillage["add"][n],table[quai][action][n])
+	end
+end
+
+function arri(voie)
+	repeat
+		nbQuai = giveQuai()
+		print(nbQuai)
+		os.sleep(1)
+	until(nbQuai ~= "no station free")
+	rooting(voie,nbQuai)
+	r.setBundledOutput(5,gare["voie"]["entre"][voie]["bloc"],255)
+	os.sleep(5)
+	r.setBundledOutput(5,gare["voie"]["entre"][voie]["bloc"],0)
+end
+
+function filtre(name,add,Id)
+	local cond = false
+	if name == "minecart" then
+		if Id == "railcraft.cart.loco.creative" then
+			if cond == false then
+				for n = 1, gare["nbVoieE"] do 
+					if gare["voie"]["entre"][n]["detec"] == add:sub(1,4) then
+						action = "arrive"
+						arri(n)
+						cond = true
+					end 
+				end
+			end
+			if cond == false then
+				for n = 1, gare["nbVoieE"] do 
+					if gare["voie"]["sorti"][n]["detec"] == add:sub(1,4) then
+						cond = true
+					end 
+				end
+			end
+		end
+	end
+end
+
+gare 		= getFile("gare.json")
+table 		= getFile("table.json")
+aiguillage  = getFile("aiguillage.json")
+
+while true do 
+	checkQuai()
+	event.pullFiltered(2,filtre)
+	print(gare["quai"][1]["full"])
+	print(gare["quai"][2]["full"])
+end
